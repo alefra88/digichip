70715b1a07b631a796b67f8e66c30fa78ed75257
alefra88 <81286342+alefra88@users.noreply.github.com>
2022-01-03 00:13:03 -0600

    Add files via upload

---
+++ procesos/submit.php
@@ -0,0 +1,28 @@
+<?php 
+
+
+
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$telefono = $_POST['tel'];
+$mensaje = $_POST['mensaje'];
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
+ <h3>
+ 	<?php echo $mensaje ?>
+ </h3>
+<br>
+<a href="../contacto.html">Regresar</a>
